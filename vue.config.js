9c82aef87b28857ec88eb623b6eb00486ba01f26
qytayh <13255238145@163.com>
2021-12-13 14:30:08 +0800

    chore:auto build page

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const Components = require('unplugin-vue-components/webpack')
 const { VarletUIResolver } = require('unplugin-vue-components/resolvers')
 module.exports = {
-    publicPath:"/",
-    outputDir: "dist",
     configureWebpack: {
         plugins: [
             Components({
