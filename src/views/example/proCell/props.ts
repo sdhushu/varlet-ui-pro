2fe3b1c3772c9ad2037f0be16551ac3c4ee74d22
EmberSpirit <wangKBweb@163.com>
2021-12-26 18:05:23 +0800

    feat: cell组件设计

---
+++ src/views/example/proCell/props.ts
@@ -0,0 +1,11 @@
+export const props2 = {
+  contents: {
+    type: Array
+  },
+  loop: {
+    type: Boolean
+  },
+  autoplay: {
+    type: Number
+  }
+}
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import dark from '@varlet/ui/es/themes/dark'
 import { StyleProvider } from '@varlet/ui'
+import proCell from '../example/proCell/index.vue'
 let current = $ref(null)
 const changeMode = () => {
   current = current?null:dark
@@ -11,6 +12,11 @@ const changeMode = () => {
 <template>
   <div>home</div>
   <var-button type='primary' @click='changeMode'>test dark</var-button>
+  <proCell :contents="['我是火猫', '你是小周']" :loop="false" :autoplay="0">
+    <template #extra>
+      <var-icon name="information" />
+    </template>
+  </proCell>
 </template>
 
 <style>
